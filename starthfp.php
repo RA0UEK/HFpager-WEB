b635ba7fe2cf262bfb79bac5beb219a654d1290d
RA0UEK <165857603+RA0UEK@users.noreply.github.com>
2024-11-15 11:39:30 +0900

    Update starthfp.php

--- starthfp.php
+++ starthfp.php
@@ -1,5 +1,4 @@
 <?php
-//$command = 'am start --user 0 -n ru.radial.nogg.hfpager/ru.radial.full.hfpager.MainActivity -a "android.intent.action.SEND" --es "android.intent.extra.TEXT" "'.$message.'" -t "text/plain" --ei "android.intent.extra.INDEX" "'.$id.'" --es "android.intent.extra.SUBJECT" "Flags:1,0"';
 $command = 'am start --user 0 -n ru.radial.nogg.hfpager/ru.radial.full.hfpager.MainActivity -a "android.intent.action.SEND" --es "android.intent.extra.TEXT" "notext" -t "text/plain" --ei "android.intent.extra.INDEX" "99999" --es "android.intent.extra.SUBJECT" "Flags:1,0"';
 shell_exec($command);
 exit();
